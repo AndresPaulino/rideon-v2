e5c7bbdd07e7ba67e9a4da21c184aaa412dcc953
Kykeon <paulinoandresmiguel@gmail.com>
2022-12-12 16:04:00 -0500

    Create Routes.js and map through pagesRoutes in App.js

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes as ReactRoutes, Route, Navigate } from 'react-router-dom';
+import pagesRoutes from './pages/routes';
+
+const Routes = () => {
+  return (
+    <ReactRoutes>
+      {pagesRoutes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.renderer()} />
+      ))}
+      <Route path='*' element={<Navigate replace to='/not-found-cover' />} />
+    </ReactRoutes>
+  );
+};
+
+export default Routes;
