886c1fb9ff6cc042719018010be6d4b92ba7501d
Kykeon <paulinoandresmiguel@gmail.com>
2022-12-12 16:13:13 -0500

    Install date-fns

---
+++ src/theme/palette.js
@@ -0,0 +1,67 @@
+export const light = {
+  alternate: {
+    main: '#c4e6f4',
+    dark: '#c4e6e4',
+  },
+  cardShadow: 'rgba(44.2, 25, 73, .11)',
+  mode: 'light',
+  primary: {
+    main: '#7a38b9',
+    light: '#d4bceb',
+    dark: '#2f6ad9',
+    contrastText: '#fff',
+  },
+  secondary: {
+    light: '#6fbddd',
+    main: '#08a0e0',
+    dark: '#0112c0',
+    contrastText: '#fff',
+  },
+  text: {
+    primary: '#1e2022',
+    secondary: '#677788',
+  },
+  divider: 'rgba(0, 0, 0, 0.12)',
+  background: {
+    paper: '#ffffff',
+    default: '#ffffff',
+    level2: '#f5f5f5',
+    level1: '#ffffff',
+  },
+};
+
+export const dark = {
+  alternate: {
+    main: '#1a2138',
+    dark: '#151a30'
+  },
+  cardShadow: 'rgba(0, 0, 0, .11)',
+  common: {
+    black: '#000',
+    white: '#fff'
+  },
+  mode: 'dark',
+  primary: {
+    main: '#1976d2',
+    light: '#2196f3',
+    dark: '#0d47a1',
+    contrastText: '#fff'
+  },
+  secondary: {
+    light: '#FFEA41',
+    main: '#FFE102',
+    dark: '#DBBE01',
+    contrastText: 'rgba(0, 0, 0, 0.87)'
+  },
+  text: {
+    primary: '#EEEEEF',
+    secondary: '#AEB0B4'
+  },
+  divider: 'rgba(255, 255, 255, 0.12)',
+  background: {
+    paper: '#222B45',
+    default: '#222B45',
+    level2: '#333',
+    level1: '#2D3748'
+  }
+};
