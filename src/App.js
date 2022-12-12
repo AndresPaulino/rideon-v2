c2e9c8696bd1e1f2e604573fd611a866f4772955
Kykeon <paulinoandresmiguel@gmail.com>
2022-12-12 16:08:35 -0500

    Install mui x-date-pickers and add to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Page from './components/Page';
+import Routes from './Routes';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </LocalizationProvider>
+    </Page>
   );
 }
 
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes as ReactRoutes, Route, Navigate } from 'react-router-dom';
+import Page from './components/Page';
 import pagesRoutes from './pages/routes';
 
 const Routes = () => {
