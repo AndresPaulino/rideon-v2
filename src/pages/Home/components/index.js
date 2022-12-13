fbb42d248a3384c5b00b092e7e07485a8318824c
Kykeon <paulinoandresmiguel@gmail.com>
2022-12-13 13:08:56 -0500

    Stage commit

--- src/pages/Home/components/index.js
+++ src/pages/Home/components/index.js
@@ -2,3 +2,4 @@ export { default as Customization } from './Customization';
 export { default as FindRide } from './FindRide';
 export { default as Hero } from './Hero';
 export { default as Hub } from './Hub';
+
