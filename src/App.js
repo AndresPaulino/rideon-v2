b9c4c42a9f9124c9cdff333ceee7640b69fd5b03
Kykeon <paulinoandresmiguel@gmail.com>
2022-12-13 13:37:49 -0500

    Install firebase deps and create .env variables

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Page from './components/Page';
 import Routes from './Routes';
+import { AuthProvider } from 'context/FirebaseContext';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Page>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-      </LocalizationProvider>
-    </Page>
+    <AuthProvider>
+      <Page>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <BrowserRouter>
+            <Routes />
+          </BrowserRouter>
+        </LocalizationProvider>
+      </Page>
+    </AuthProvider>
   );
 }
 
