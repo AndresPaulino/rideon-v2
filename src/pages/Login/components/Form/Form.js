a8775ca426da99c4672829e87ff7dffaf39d2523
Kykeon <paulinoandresmiguel@gmail.com>
2022-12-19 20:26:18 -0500

    Test login function

--- src/pages/Login/components/Form/Form.js
+++ src/pages/Login/components/Form/Form.js
@@ -1,51 +1,62 @@
-/* eslint-disable react/no-unescaped-entities */
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-
+import { useState } from 'react';
+import * as Yup from 'yup';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+// form
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+// @mui
+import { Link, Stack, Alert, IconButton, InputAdornment, Typography } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+// auth
 import { useAuthContext } from 'hooks/useAuthContext';
+// components
+import Iconify from 'components/iconify';
+import FormProvider, { RHFTextField } from 'components/hook-form';
+import { Box } from '@mui/system';
 
-const validationSchema = yup.object({
-  email: yup.string().trim().email('Please enter a valid email address').required('Email is required.'),
-  password: yup
-    .string()
-    .required('Please specify your password')
-    .min(8, 'The password should have at minimum length of 8'),
-});
+// ----------------------------------------------------------------------
 
-const Form = () => {
-  const { login } = useAuthContext();
+export default function AuthLoginForm() {
   const navigate = useNavigate();
-  const initialValues = {
+  const { login } = useAuthContext();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().required('Email is required').email('Email must be a valid email address'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const defaultValues = {
     email: '',
     password: '',
   };
 
-  const onSubmit = (values) => {
-    const { email, password } = values;
-
-    login(email, password).then(() => {
-      navigate('/dashboard', { replace: true });
-    });
-    formik.resetForm();
-    
-
-    
-    navigate('/login', { replace: true });
-  };
-
-  const formik = useFormik({
-    initialValues,
-    validationSchema: validationSchema,
-    onSubmit,
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
   });
+
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      await login(data.email, data.password);
+      navigate('/', { replace: true });
+    } catch (error) {
+      console.error(error);
+      reset();
+      setError('afterSubmit', {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
 
   return (
     <Box bgcolor={'alternate.main'} padding={{ xs: 4, sm: 6 }} borderRadius={2}>
@@ -68,102 +79,55 @@ const Form = () => {
         >
           Welcome back
         </Typography>
-        <Typography color='text.secondary'>Login to manage your account and start riding.</Typography>
+        <Typography color='text.secondary'>Login with your email and password.</Typography>
       </Box>
-      <form onSubmit={formik.handleSubmit}>
-        <Grid container spacing={4}>
-          <Grid item xs={12}>
-            <Typography variant={'subtitle2'} sx={{ marginBottom: 2 }}>
-              Enter your email
-            </Typography>
-            <TextField
-              label='Email *'
-              variant='outlined'
-              name={'email'}
-              fullWidth
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Box
-              display='flex'
-              flexDirection={{ xs: 'column', sm: 'row' }}
-              alignItems={{ xs: 'stretched', sm: 'center' }}
-              justifyContent={'space-between'}
-              width={1}
-              marginBottom={2}
-            >
-              <Box marginBottom={{ xs: 1, sm: 0 }}>
-                <Typography variant={'subtitle2'}>Enter your password</Typography>
-              </Box>
-              <Typography variant={'subtitle2'}>
-                <Link
-                  component={'a'}
-                  color={'primary'}
-                  href={'/password-reset'}
-                  underline={'none'}
-                  sx={{
-                    ':hover': {
-                      textDecoration: 'underline',
-                    },
-                  }}
-                >
-                  Forgot your password?
-                </Link>
-              </Typography>
-            </Box>
-            <TextField
-              label='Password *'
-              variant='outlined'
-              name={'password'}
-              type={'password'}
-              fullWidth
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-            />
-          </Grid>
-          <Grid item container xs={12}>
-            <Box
-              display='flex'
-              flexDirection={{ xs: 'column', sm: 'row' }}
-              alignItems={{ xs: 'stretched', sm: 'center' }}
-              justifyContent={'space-between'}
-              width={1}
-              maxWidth={600}
-              margin={'0 auto'}
-            >
-              <Box marginBottom={{ xs: 1, sm: 0 }}>
-                <Typography variant={'subtitle2'}>
-                  Don't have an account yet?{' '}
-                  <Link
-                    component={'a'}
-                    color={'primary'}
-                    href={'/register'}
-                    underline={'none'}
-                    sx={{
-                      ':hover': {
-                        textDecoration: 'underline',
-                      },
-                    }}
-                  >
-                    Register here.
-                  </Link>
-                </Typography>
-              </Box>
-              <Button size={'large'} variant={'contained'} type={'submit'}>
-                Login
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </form>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+          {!!errors.afterSubmit && <Alert severity='error'>{errors.afterSubmit.message}</Alert>}
+
+          <RHFTextField name='email' label='Email address' />
+
+          <RHFTextField
+            name='password'
+            label='Password'
+            type={showPassword ? 'text' : 'password'}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton onClick={() => setShowPassword(!showPassword)} edge='end'>
+                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Stack>
+
+        <Stack alignItems='flex-end' sx={{ my: 2 }}>
+          <Link component={RouterLink} to={'/'} variant='body2' color='inherit' underline='always'>
+            Forgot password?
+          </Link>
+        </Stack>
+
+        <LoadingButton
+          fullWidth
+          color='inherit'
+          size='large'
+          type='submit'
+          variant='contained'
+          loading={isSubmitting}
+          sx={{
+            bgcolor: 'text.primary',
+            color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800'),
+            '&:hover': {
+              bgcolor: 'text.primary',
+              color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800'),
+            },
+          }}
+        >
+          Login
+        </LoadingButton>
+      </FormProvider>
     </Box>
   );
-};
-
-export default Form;
+}
